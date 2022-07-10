354f442d2b15e4270a2a8594e531a55b46a06c66
xiaoming <2604863701@qq.com>
2022-07-10 11:30:40 +0800

    项目 element ui 处理弹出框

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="hello">
-    <div>hello</div>
+    <h1>hello</h1>
+    <el-row>
+      <el-button @click="open">打开</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
@@ -9,11 +17,15 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods:{
+    open(){
+      this.$message('这是一条消息提示信息！！')
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
